b0db0a9f31bd7a2ff535437df9a8f62f88144545
Jason R <90394027+The1Riv123@users.noreply.github.com>
2021-10-28 12:04:51 -0400

    Create variable_practice.py

---
+++ variable_practice.py
@@ -0,0 +1,11 @@
+# varioble practice, jason rivers, 10/28/21 11:40am, version 0.0
+
+# varible types -- most common
+
+my_float = 1.535
+my_int = -1231454345
+my_string = "I like trains."
+
+print(my_float)
+print(my_int)
+print(my_string)
